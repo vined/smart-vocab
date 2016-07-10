8bb887c40c7d33df1e70b5bf59aecfa1551bd19e
Edvinas Naraveckas <e.naraveckas@gmail.com>
2016-07-10 22:23:08 +0300

    Reading vocabulary from file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
+import System.Environment
+import System.IO
+import Data.List
 import Lib
 
+add :: IO ()
+add = putStrLn "TBD"
+
+testWords :: IO ()
+testWords = do
+    withFile "words.txt" ReadMode (\handle -> do
+        contents <- hGetContents handle
+        print . getVocab $ lines contents)
+
+
+dispatch :: [(String, IO ())]
+dispatch = [ ("add", add)
+           , ("test", testWords)
+           ]
+
 main :: IO ()
-main = someFunc
+main = do
+    (command:args) <- getArgs
+    let (Just action) = lookup command dispatch
+    action
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,18 @@
 module Lib
-    ( someFunc
+    ( getVocab
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Strings
+
+data VocabEntry = VocabEntry { english :: String
+                             , italian :: String
+                             } deriving (Show)
+
+toVocabEntry :: [String] -> VocabEntry
+toVocabEntry (enWord:itWord:tail) = VocabEntry {english=enWord, italian=itWord}
+
+splitBySemicolon :: String -> [String]
+splitBySemicolon = strSplitAll ";"
+
+getVocab :: [String] -> [VocabEntry]
+getVocab entries = map toVocabEntry $ map splitBySemicolon entries
